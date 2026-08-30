d89a620f25952c799582a3726360f032f5ce0853
jwhong70 <jwhong70@gmail.com>
2026-08-30 16:37:18 +0900

    analysis2

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -379,9 +379,9 @@ export default function Filter({ initialStocks, isPremium }: FilterProps) {
                   </th>
                   <th
                     onClick={() => handleSort(`yield_${selectedPeriod}` as any)}
-                    className="py-3 px-3 sm:px-6 text-right cursor-pointer hover:bg-gray-900 transition-colors w-36 sm:w-44"
+                    className="py-3 px-3 sm:px-6 text-right cursor-pointer hover:bg-gray-900 transition-colors w-24 sm:w-32"
                   >
-                    수익률 ({selectedPeriod === '1w' ? '1주' : selectedPeriod === '5w' ? '5주' : selectedPeriod === '20w' ? '20주' : selectedPeriod === '60w' ? '60주' : '120주'}) {renderSortIcon(`yield_${selectedPeriod}` as any)}
+                    수익률 {renderSortIcon(`yield_${selectedPeriod}` as any)}
                   </th>
                 </tr>
               </thead>
